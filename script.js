1ce4a9e1aeabd789e93187734f2b7ef7cbb8b12c
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-09-15 12:27:28 +0200

    create grid for schedule

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let NoWeeks = 4;
+let noWeeks = 4;
+let scheduleGrid = document.getElementById("schedule")
 
-for (let i=0; i<= noWeeks *7; i++) {
-
+for (let i=0; i < noWeeks *7; i++) {
+    let dayDiv = document.createElement("div");
+    dayDiv.classList.add("day")
+    if (i%6 == 0) {
+        dayDiv.classList.add("weekend");
+    } else {
+        dayDiv.classList.add("weekday");
+    }
+    scheduleGrid.appendChild(dayDiv);
 }
